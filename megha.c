3cf2cd4cde1bb916c57bc453d420277e19070160
hlmeghana <51438562+hlmeghana@users.noreply.github.com>
2019-06-06 14:36:14 +0530

    Create megha.c

---
+++ megha.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void main()
+{
+	 input(); 
+	add();
+	output();
+}
+int input(int a,int b)
+{
+	int a,b;
+	printf("enter the first number:");
+	scanf("%d",&a);
+	printf("enter the second number:");
+	scanf("%d",&b);
+return (a,b);
+	
+int add()
+{
+ 	int sum;	
+ 	sum=a+b;
+return sum;
+}
+int  output()
+{
+	printf("addition of two numbers is:%d",sum);
+return 0;
+}
+
+
+	
